15c51b876e6f550b53f4390949b6c43471c7e16a
Chilla Kalyan <127284726+chillakalyan@users.noreply.github.com>
2026-03-09 12:55:35 +0530

    Update capture_checkpoint_state.py

--- scripts/capture_checkpoint_state.py
+++ scripts/capture_checkpoint_state.py
@@ -1,1 +1,26 @@
 
+import subprocess
+
+def capture_checkpoint_state():
+
+    gdb_commands = """
+target remote :1234
+info registers
+p/x $pc
+p/x $mstatus
+p/x $satp
+p/x $mtvec
+p/x $mepc
+p/x $medeleg
+p/x $mideleg
+quit
+"""
+
+    with open("checkpoint_state.gdb", "w") as f:
+        f.write(gdb_commands)
+
+    subprocess.run(["gdb-multiarch", "-x", "checkpoint_state.gdb"])
+
+
+if __name__ == "__main__":
+    capture_checkpoint_state()
